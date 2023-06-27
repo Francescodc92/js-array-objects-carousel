5cc816f94275c1af6a94909ea8602dc5fcdf3889
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-27 21:02:27 +0200

    end adaptation to new elements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 /*
+ - milestone0. Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+    
+  - milestone1. Ora rimuoviamo i contenuti statici e usiamo l'array di oggetti letterali per popolare dinamicamente il carosello.
+  Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+  
+  - milestone2. Aggiungere il "ciclo infinito" del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
 
+  RAGIONAMENTO BASE:
+  1. refatturare il codice in maniera da farlo funzionare con la nuova struttura di dati cosi facendo praticamente le prime milestone saranno completate 
+  2. modificare il rendering dell'img aggiungendo all'img grande il testo 
 
 */
-
+/*-------------
+DATA
+------------- */
 const images = [
     {
         image: 'img/01.webp',
@@ -30,3 +41,103 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+/*-------------
+Global variables
+------------- */
+const carouselContainer = document.querySelector(".img-container");
+const thumbnailContainer = document.querySelector(".container-thumbnails");
+let activeImg = 1
+const prevArrow = document.querySelector('#left')
+const nextArrow = document.querySelector('#right')
+
+const createHTMLElement = (currentImage, index) => {
+  const imgWrapper = document.createElement("div");
+  imgWrapper.classList.add("img-wrapper");
+  
+  let imgElement = `
+    <img src="${currentImage.image}" class="img img_${index + 1}
+  `
+  if (index == 0) {
+    imgElement += " active"
+  }
+  imgElement += ` alt="immagine ${index + 1}" />`
+
+
+  carouselContainer.innerHTML += imgElement;
+  const imageTitle = document.createElement('h3');
+  imageTitle.innerHTML = currentImage.title;
+
+  const imageDescription = document.createElement('p');
+  imageDescription.innerHTML = currentImage.text;
+  
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('text-container', `text_${index + 1}`);
+  if(index == 0){
+    textContainer.classList.add('active')
+  };
+  textContainer.append(imageTitle, imageDescription);
+  
+  carouselContainer.append(textContainer);
+
+  imgWrapper.innerHTML += imgElement;
+  thumbnailContainer.append(imgWrapper);
+};
+
+
+prevArrow.addEventListener('click', () => {
+  const activeImgBox = document.querySelectorAll(".img_" + activeImg)
+  const activeText = document.querySelector('.text_' + activeImg)
+
+
+  activeImgBox.forEach((img) => {
+    img.classList.remove("active")
+  })
+  activeText.classList.remove('active')
+
+  if (activeImg == 1) {
+    activeImg = images.length
+  } else {
+    activeImg--
+  }
+  const previousImgBox = document.querySelectorAll(".img_" + activeImg)
+  const previousText = document.querySelector('.text_' + activeImg)
+
+  
+  previousImgBox.forEach((img) => {
+    img.classList.add("active")
+  })
+  previousText.classList.add('active')
+  
+})
+
+nextArrow.addEventListener('click', () => {
+  const activeImgBox = document.querySelectorAll(".img_" + activeImg)
+  const activeText = document.querySelector('.text_' + activeImg)
+
+
+  activeImgBox.forEach((img) => {
+    img.classList.remove("active")
+  })
+  activeText.classList.remove('active')
+
+ if (activeImg == images.length) {
+    activeImg = 1
+  } else {
+    activeImg++
+  }
+  const previousImgBox = document.querySelectorAll(".img_" + activeImg)
+  const previousText = document.querySelector('.text_' + activeImg)
+
+  
+  previousImgBox.forEach((img) => {
+    img.classList.add("active")
+  })
+  previousText.classList.add('active')
+  
+})
+
+images.forEach((currentImage, index)=> {
+  createHTMLElement(currentImage, index);
+});  
+
+
